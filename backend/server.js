4f0f2f8fcd7e5a81431aa81010aeaa6f63aaa4fa
Biochem <stefanovitteri@system-1.local>
2023-05-25 06:14:48 -0400

    second commit

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,20 @@ app.all('*', (req, res) => {
     }
 });
 
+// serve frontend 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => 
+      res.sendFile(
+        path.resolve(__dirname, '../', frontend, 'build', 'index.html')
+
+      )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
